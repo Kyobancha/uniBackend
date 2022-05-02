d5726affc00dd6e3840f462dd8119e7e8ef3a5a6
Timo <timo.huynh@tutamail.com>
2022-05-02 23:24:37 +0200

    worked on HTTP error codes

--- endpoints/user/UserService.js
+++ endpoints/user/UserService.js
@@ -4,49 +4,100 @@ const { User } = require("./UserModel");
 function create(req, res){
     const user = new User({
         userID: req.body.userID,
-        userName: req.body.userName,
-        password: req.body.password,
-        isAdministrator: req.body.isAdministrator
+        userName: req.body.userName ? req.body.userName : req.body.userID,
+        password: req.body.password ? req.body.password : "",
+        isAdministrator: req.body.isAdministrator ? req.body.isAdministrator : false
     });
-    user.save().then(() => console.log('User saved'))
-    // res.setHeader('Content-Type', 'application/json')
-    res.send(user);
-}
 
-function getAll(res){
-    User.find(function (err, docs) {
+    User.findOne({userID: req.body.userID}, function (err, doc) {
         if(err){
-            res.setHeader("HTTP", "Bad Request"); //overwork
-        }
-        res.send(docs)
-    });
-}
-
-function get(req, res){
-    User.findOne({userID: req.params.userID}, function (err, docs) {
-        if(err){
-            res.setHeader("HTTP", "Bad Request"); //overwork
+            res.status(500);
+            res.send("This is our fault, sorry!");
+        }else if(doc){
+            res.status(405);
+            res.send("This user ID is already taken. Please choose another one.");
         } else {
-            res.send(docs)
+            user.save()
+            .then(() => {
+                res.status(201);
+                res.send(user);
+                console.log('User saved')
+            })
+            .catch(error => {
+                res.status(400);
+                res.send("A user ID is required");
+                console.log(error);
+            })
         }
     })
 }
 
-function update(req, res){
-    let user = User.updateOne({userID: req.params.userID}, req.body, function (err, docs) {
-        if(err){
-            res.setHeader("HTTP", "Bad Request"); //overwork
+function getAll(res){
+    User.find()
+    .then(docs => {
+        res.send(docs)
+    })
+    .catch(err => {
+        res.status(500);
+        res.send("This is our fault, sorry!");
+        console.log(err)
+    })
+}
+
+function get(req, res){
+    User.findOne({userID: req.params.userID})
+    .then(doc => {
+        if(doc){
+            res.send(doc);
+        } else{
+            res.status(404);
+            res.send("This user doesn't exit.");
         }
-        user = req.body;
+    })
+    .catch(err => {
+        res.status(500);
+        res.send("This is our fault, sorry!");
+        console.log(err);
+    })
+}
+
+//TODO was ist, wenn im body ein fehlerhafter key steht?
+function update(req, res){
+    User.findOne({userID: req.params.userID})
+    .then(doc => {
+        if(doc){
+            return User.updateOne({userID: req.params.userID}, req.body)
+        } else{
+            res.status(403);
+            throw new Error({status: 403});
+        }
+    })
+    .then(() => {
+        res.status(204);
+        res.send("");
         console.log('User updated');
-        res.send(docs) //EVTL etwas anderes schicken
-    });
+    })
+    .catch(err => {
+        if(err.status = 403){
+            res.send("No user with the provided id exits so far.");
+        } else{
+            res.status(500);
+            res.send("This is our fault, sorry!");
+            console.log(err);
+        }
+    })
 }
 
 function remove(req, res){
-    User.deleteOne({userID: req.params.userID}, (err, docs) => {
+    User.deleteOne({userID: req.params.userID})
+    .then(doc => {
+        res.send(doc)
         console.log("User deleted");
-        res.send(docs)
+    })
+    .catch(err => {
+        res.status(500);
+        res.send("This is our fault, sorry!");
+        console.log(err);
     })
 }
 
