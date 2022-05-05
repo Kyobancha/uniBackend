619f71ada809148675bab2bf466d53f60e9b5039
Timo <huynh@Timos-MacBook-Air.local>
2022-05-05 14:15:52 +0200

    added config file

--- httpServer.js
+++ httpServer.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = require('./endpoints/user/PublicUserRoute');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('config');
 
-mongoose.connect('mongodb://localhost:27017/WebII', {useNewUrlParser: true, useUnifiedTopology: true});
+const dbConnectionString = config.get('db.connectionString')
+const dbUseNewUrlParser = config.get('db.connectionOptions.useNewUrlParser');
+const dbUseUnifiedTopology = config.get('db.connectionOptions.useUnifiedTopology');
+
+mongoose.connect(dbConnectionString, {useNewUrlParser: dbUseNewUrlParser, useUnifiedTopology: dbUseUnifiedTopology});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() { console.log("Conntected"); });
